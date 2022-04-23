a7c1bebc5cc295ec30635888948140f03cad2722
joyjsmun <joy.js.mun@gmail.com>
2022-04-23 22:36:36 +0900

    added contextProvider

---
+++ src/Context.js
@@ -0,0 +1,13 @@
+import React,{createContext} from "react"
+
+const Context = createContext()
+
+function ContextProvider({children}){
+    return(
+        <Context.Provider value="">
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {ContextProvider,Context}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {ContextProvider } from './Context';
 import {BrowserRouter as Router} from "react-router-dom"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ContextProvider>
     <Router><App /></Router>
+    </ContextProvider>
   </React.StrictMode>
 );
