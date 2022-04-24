4dd6c5bc7ea0f0eae4c4ec54b5400a451ae069da
joyjsmun <joy.js.mun@gmail.com>
2022-04-24 23:20:24 +0900

    added fetching data

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,17 @@
-import React,{createContext,useState} from "react"
+import React,{createContext,useState,useEffect} from "react"
 
 const Context = createContext()
 
+
+
 function ContextProvider({children}){
     const [allPhotos,setAllPhotos] = useState([])
+
+    useEffect(()=>{
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+        .then(res => res.json())
+        .then(data => console.log(data) )
+    },[])
     return(
         <Context.Provider value={{allPhotos}}>
             {children}
