8b5e375b09b6a036f496e85cc267f8dbbf9af516
joyjsmun <joy.js.mun@gmail.com>
2022-04-26 21:42:49 +0900

    added image function

---
+++ src/components/Image.js
@@ -0,0 +1,9 @@
+function Image({className}){
+    return(
+        <div className={`${className} image-container`}>
+            <img src={""} className="image-grid"  />
+        </div>       
+    )
+}
+
+export default Image
