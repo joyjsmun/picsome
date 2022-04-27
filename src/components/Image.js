89023262a51cad166eef2f272bced384879e14dd
joyjsmun <joy.js.mun@gmail.com>
2022-04-27 23:44:23 +0900

    added challenge

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,3 +1,14 @@
+/* # Challenge
+
+On the Image component, track the hover state
+
+1. Create state boolean for "hovered"
+2. When the mouse enters the Image's div, set "hovered" to true
+3. When the mouse leaves the Image's div, set "hovered" to false
+4. Log "hovered" to the console so you know it's changing successfully. */
+
+
+
 import React from "react"
 
 function Image({className, img}) {
