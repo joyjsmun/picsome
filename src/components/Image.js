cecaecd8a5eea05750ed9802e459a4989c981d92
joyjsmun <joy.js.mun@gmail.com>
2022-04-29 00:04:34 +0900

    added function to start

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,12 +1,4 @@
-/* # Challenge
 
-Conditionally render the heart and plus icons when an Image component is being hovered on
-
-1. Icon to render for the heart:
-<i className="ri-heart-line favorite"></i>
-
-2. Icon to render for the plus:
-<i className="ri-add-circle-line cart"></i> */
 
 import React, { useState } from "react"
 
