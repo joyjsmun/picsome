46349794fce28fa5281baf181f30a1d9d6ef8209
joyjsmun <joy.js.mun@gmail.com>
2022-04-28 23:48:42 +0900

    added challenge for the hovering

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,23 +1,19 @@
 /* # Challenge
 
-On the Image component, track the hover state
+Conditionally render the heart and plus icons when an Image component is being hovered on
 
-1. Create state boolean for "hovered"
-2. When the mouse enters the Image's div, set "hovered" to true
-3. When the mouse leaves the Image's div, set "hovered" to false
-4. Log "hovered" to the console so you know it's changing successfully. */
+1. Icon to render for the heart:
+<i className="ri-heart-line favorite"></i>
 
-
+2. Icon to render for the plus:
+<i className="ri-add-circle-line cart"></i> */
 
 import React, { useState } from "react"
 
 function Image({className, img}) {
     const [hovered,setHovered] = useState(false)
 
-    function handleHover(){
-        setHovered(prev => !prev)
-        console.log(hovered)
-    }
+   console.log(hovered)
 
     return (
         <div className={`${className} image-container`}>
