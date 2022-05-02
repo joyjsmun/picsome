844de0416829c050ca0acb0d98a57e40e6261193
joyjsmun <joy.js.mun@gmail.com>
2022-05-03 00:54:14 +0900

    added checkout

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Context } from "../Context"
+import CartItem from "../components/CartItem"
 
 function Cart() {
+    const {cartItems} = useContext(Context)
+    const cartElements = cartItems.map(item => (
+        <CartItem item={item} key={item.key} />
+    ))
     return (
         <main className="cart-page">
-            <h1>Check out</h1>
+            <h1>Check out</h1>´
+            <p className="total-cost">Total: </p>
+            <div className="order-button">
+                <button>Place Order</button>
+            </div>
+            {cartElements}
         </main>
     )
 }
