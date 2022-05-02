57cfb5a76a800203a15815139410fc8293a3fe02
joyjsmun <joy.js.mun@gmail.com>
2022-05-02 22:55:23 +0900

    added cart state

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,11 @@
+/* # Challenge
 
+Setup context to manage items in an array called `cartItems`. This will be an array of image objects.
+
+1. Add the `cartItems` state to context. (Array)
+2. Add function to add an image to the cart. (Takes the full image object as parameter)
+3. Make it so clicking the plus icon on the image adds the item to the cart. (Console.log the cart items array to see that it's working)
+ */
 
 import React,{createContext,useState,useEffect} from "react"
 
@@ -8,6 +15,7 @@ const Context = createContext()
 
 function ContextProvider({children}){
     const [allPhotos,setAllPhotos] = useState([])
+    const [cartItems,setCartItems] = useState([])
 
     useEffect(()=>{
         fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
@@ -17,11 +25,15 @@ function ContextProvider({children}){
 
     // console.log(allPhotos)
 
+   function addItemToCart(newItem){
+       setCartItems(prevItem => [...prevItem,newItem])
+   }
+
+    console.log(cartItems)
+
     function toggleFavorite(id){
         const updatedPhoto = allPhotos.map(photo => {
             if(photo.id === id){
-                console.log(id)
-                console.log(!photo.isFavorite)
                 return { ...photo, isFavorite: !photo.isFavorite}
             }
             return photo
@@ -30,7 +42,7 @@ function ContextProvider({children}){
     }
 
     return(
-        <Context.Provider value={{allPhotos,toggleFavorite}}>
+        <Context.Provider value={{allPhotos,toggleFavorite,addItemToCart}}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -4,10 +4,10 @@ import PropTypes from "prop-types"
 import React, { useState,useContext } from "react"
 
 function Image({className, img}) {
-    const {toggleFavorite} = useContext(Context)
+    const {toggleFavorite,addItemToCart} = useContext(Context)
     const [hovered,setHovered] = useState(false)
     const heartIcon = hovered && <i className={img.isFavorite === true ? "ri-heart-fill favorite" : "ri-heart-line favorite"} onClick={() => toggleFavorite(img.id)}></i> 
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart" onClick={() => addItemToCart(img)}></i>
 
     return (
         <div className={`${className} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
