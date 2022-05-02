9e67b6d59a279042ff988a7fef216cd1a4aecb09
joyjsmun <joy.js.mun@gmail.com>
2022-05-02 22:16:09 +0900

    added favoriet display

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,10 +5,8 @@ import React, { useState,useContext } from "react"
 function Image({className, img}) {
     const {toggleFavorite} = useContext(Context)
     const [hovered,setHovered] = useState(false)
-    const heartIcon = hovered &&  
-    <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+    const heartIcon = hovered && <i className={img.isFavorite === true ? "ri-heart-fill favorite" : "ri-heart-line favorite"} onClick={() => toggleFavorite(img.id)}></i> 
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
-    
 
     return (
         <div className={`${className} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
