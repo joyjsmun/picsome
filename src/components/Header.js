a7dade4f1a8e2728d8aba12667601020d9898f0f
joyjsmun <joy.js.mun@gmail.com>
2022-05-03 00:05:53 +0900

    updated cart logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,18 +16,13 @@ Full class name to use WHEN CART IS EMPTY:
 
 function Header() {
     const {cartItems} = useContext(Context) 
-    function cartIcon(){
-        if(cartItems.length === 0){
-            return <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
-        }else{
-            return <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
-        }
-    }
+    const cartIcon = cartItems.length > 0 ? <i className="ri-shopping-cart-fill ri-fw ri-2x"></i> :
+    <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
 
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart">{cartIcon()}</Link>
+            <Link to="/cart">{cartIcon}</Link>
         </header>
     )
 }
